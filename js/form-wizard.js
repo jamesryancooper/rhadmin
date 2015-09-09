c54828a207e57029611c926d99bbda95847d8fea
jamesryancooper <jamesryancooper@gmail.com>
2015-09-09 13:28:39 -0500

    Add Wizard HTML

--- js/form-wizard.js
+++ js/form-wizard.js
@@ -6,72 +6,34 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('#rootwizard1').bootstrapWizard({
+    $('#create-blueprint-wizard').bootstrapWizard({
         onNext: function (tab, navigation, index) {
             if (index == 2) {
                 // Make sure we entered the name
-                if (!$('#name').val()) {
-                    alert('You must enter your name');
-                    $('#name').focus();
+                if (!$('#blueprint-wizard-keyword').val()) {
+                    swal("Oops!", "You must enter a keyword!");
+                    $('#blueprint-wizard-keyword').focus();
                     return false;
+                } else {
+
                 }
             }
             // Set the name for the next tab
-            $('#tabs3').html('Hello <b>' + $('#name').val() + '</b>, <br> Hope you are doing great today!<br> Well you can see the value from previous step is being displayed here. We request you to mention your location on next step to elplore it more and more!!!');
+            $('#tabs3').html('Enter a target location for the <b>' + $('#blueprint-wizard-keyword').val() + '</b> blueprint');
 
-            // Set the location for the next tab
-            var location = $('#location').val();
-            console.log(location);
-            if (location) {
-                $('#tabs5').html('Hello <b class="text-info">' + $('#name').val() + '</b>, <br> We are happy to let you know that you are native of <b class="text-success">' + $('#location').val() + '</b>');
-            } else {
-                $('#tabs5').html('Hello <b class="text-info">' + $('#name').val() + '</b>, <br> We are happy to let you know that you are native of <b class="text-success"> [Location Name] </b>');
+            if (index == 3) {
+                // Make sure we entered the name
+                if (!$('#blueprint-wizard-location').val()) {
+                    swal("Oops!", "You must enter a location!");
+                    $('#blueprint-wizard-location').focus();
+                    return false;
+                } else {
+
+                }
             }
 
 
         },
-        onTabShow: function (tab, navigation, index) {
-            var $total = navigation.find('li').length;
-            var $current = index + 1;
-            var $percent = ($current / $total) * 100;
-            $('#rootwizard1 .progress-bar').css({
-                width: $percent + '%'
-            });
-        }
     });
     window.prettyPrint && prettyPrint()
-});
-
-/* for last wizard model */
-$(document).ready(function () {
-    $('#rootwizard2').bootstrapWizard({
-        'tabClass': 'piluku-tabs'
-    });
-    window.prettyPrint && prettyPrint();
-
-    // Disable step
-    $('#disable-step').on('click', function () {
-        $('#rootwizard2').bootstrapWizard('disable', $('#stepid').val());
-    });
-
-    // Enable step
-    $('#enable-step').on('click', function () {
-        $('#rootwizard2').bootstrapWizard('enable', $('#stepid').val());
-    });
-
-    // Remove step
-    $('#remove-step').on('click', function () {
-        $('#rootwizard2').bootstrapWizard('remove', $('#stepid').val(), true);
-    });
-
-    // Show step
-    $('#show-step').on('click', function () {
-        $('#rootwizard2').bootstrapWizard('display', $('#stepid').val());
-    });
-
-    // Hide step
-    $('#hide-step').on('click', function () {
-        $('#rootwizard2').bootstrapWizard('hide', $('#stepid').val());
-    });
-
 });
---
+++ js/fullscreen-overlay.js
@@ -0,0 +1,43 @@
+(function() {
+    var container = document.querySelector( 'div.content' ),
+        triggerBttn = document.getElementById( 'create-new-blueprint' ),
+        overlay = document.querySelector( 'div#create-new-blueprint-overlay' ),
+        closeBttn = overlay.querySelector( 'button.settings-overlay-close' );
+    transEndEventNames = {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+    },
+        transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+        support = { transitions : Modernizr.csstransitions };
+
+    function toggleOverlay() {
+        if( classie.has( overlay, 'open' ) ) {
+            classie.remove( overlay, 'open' );
+            classie.remove( container, 'overlay-open' );
+            classie.add( overlay, 'close' );
+            var onEndTransitionFn = function( ev ) {
+                if( support.transitions ) {
+                    if( ev.propertyName !== 'visibility' ) return;
+                    this.removeEventListener( transEndEventName, onEndTransitionFn );
+                }
+                classie.remove( overlay, 'close' );
+            };
+            if( support.transitions ) {
+                overlay.addEventListener( transEndEventName, onEndTransitionFn );
+            }
+            else {
+                onEndTransitionFn();
+            }
+        }
+        else if( !classie.has( overlay, 'close' ) ) {
+            classie.add( overlay, 'open' );
+            classie.add( container, 'overlay-open' );
+        }
+    }
+
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    closeBttn.addEventListener( 'click', toggleOverlay );
+})();
