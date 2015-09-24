33be420a02250548aa0652e0aecf6bd1ad70ce4b
jamesryancooper <jamesryancooper@gmail.com>
2015-09-24 10:58:34 -0500

    Added signin and signup pages. Moved blueprints from the index.html to their own blueprints.html page. Created a projects.html page as well.

--- js/fullscreen-overlay.js
+++ js/fullscreen-overlay.js
@@ -41,3 +41,189 @@
     triggerBttn.addEventListener( 'click', toggleOverlay );
     closeBttn.addEventListener( 'click', toggleOverlay );
 })();
+
+
+(function() {
+    var container = document.querySelector( 'div#menu-click3' ),
+        triggerBttn = document.getElementById( 'anchor-filter-url-settings-icon' ),
+        overlay = document.querySelector( 'div#anchor-filter-url-settings' ),
+        closeBttn = overlay.querySelector( 'button.settings-overlay-close' ),
+        closeAnch = overlay.querySelector( 'a.settings-overlay-close' );
+    transEndEventNames = {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+    },
+        transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+        support = { transitions : Modernizr.csstransitions };
+
+    function toggleOverlay() {
+        if( classie.has( overlay, 'open' ) ) {
+            classie.remove( overlay, 'open' );
+            classie.remove( container, 'overlay-open' );
+            classie.add( overlay, 'close' );
+            var onEndTransitionFn = function( ev ) {
+                if( support.transitions ) {
+                    if( ev.propertyName !== 'visibility' ) return;
+                    this.removeEventListener( transEndEventName, onEndTransitionFn );
+                }
+                classie.remove( overlay, 'close' );
+            };
+            if( support.transitions ) {
+                overlay.addEventListener( transEndEventName, onEndTransitionFn );
+            }
+            else {
+                onEndTransitionFn();
+            }
+        }
+        else if( !classie.has( overlay, 'close' ) ) {
+            classie.add( overlay, 'open' );
+            classie.add( container, 'overlay-open' );
+        }
+    }
+
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    closeBttn.addEventListener( 'click', toggleOverlay );
+    closeAnch.addEventListener( 'click', toggleOverlay );
+})();
+
+
+(function() {
+    var container = document.querySelector( 'div#menu-click3' ),
+        triggerBttn = document.getElementById( 'anchor-filter-exact-settings-icon' ),
+        overlay = document.querySelector( 'div#exact-filter-url-settings' ),
+        closeBttn = overlay.querySelector( 'button.settings-overlay-close' ),
+        closeAnch = overlay.querySelector( 'a.settings-overlay-close' );
+    transEndEventNames = {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+    },
+        transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+        support = { transitions : Modernizr.csstransitions };
+
+    function toggleOverlay() {
+        if( classie.has( overlay, 'open' ) ) {
+            classie.remove( overlay, 'open' );
+            classie.remove( container, 'overlay-open' );
+            classie.add( overlay, 'close' );
+            var onEndTransitionFn = function( ev ) {
+                if( support.transitions ) {
+                    if( ev.propertyName !== 'visibility' ) return;
+                    this.removeEventListener( transEndEventName, onEndTransitionFn );
+                }
+                classie.remove( overlay, 'close' );
+            };
+            if( support.transitions ) {
+                overlay.addEventListener( transEndEventName, onEndTransitionFn );
+            }
+            else {
+                onEndTransitionFn();
+            }
+        }
+        else if( !classie.has( overlay, 'close' ) ) {
+            classie.add( overlay, 'open' );
+            classie.add( container, 'overlay-open' );
+        }
+    }
+
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    closeBttn.addEventListener( 'click', toggleOverlay );
+    closeAnch.addEventListener( 'click', toggleOverlay );
+})();
+
+(function() {
+    var container = document.querySelector( 'div#menu-click3' ),
+        triggerBttn = document.getElementById( 'anchor-filter-partial-settings-icon' ),
+        overlay = document.querySelector( 'div#partial-filter-url-settings' ),
+        closeBttn = overlay.querySelector( 'button.settings-overlay-close' ),
+        closeAnch = overlay.querySelector( 'a.settings-overlay-close' );
+    transEndEventNames = {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+    },
+        transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+        support = { transitions : Modernizr.csstransitions };
+
+    function toggleOverlay() {
+        if( classie.has( overlay, 'open' ) ) {
+            classie.remove( overlay, 'open' );
+            classie.remove( container, 'overlay-open' );
+            classie.add( overlay, 'close' );
+            var onEndTransitionFn = function( ev ) {
+                if( support.transitions ) {
+                    if( ev.propertyName !== 'visibility' ) return;
+                    this.removeEventListener( transEndEventName, onEndTransitionFn );
+                }
+                classie.remove( overlay, 'close' );
+            };
+            if( support.transitions ) {
+                overlay.addEventListener( transEndEventName, onEndTransitionFn );
+            }
+            else {
+                onEndTransitionFn();
+            }
+        }
+        else if( !classie.has( overlay, 'close' ) ) {
+            classie.add( overlay, 'open' );
+            classie.add( container, 'overlay-open' );
+        }
+    }
+
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    closeBttn.addEventListener( 'click', toggleOverlay );
+    closeAnch.addEventListener( 'click', toggleOverlay );
+})();
+
+(function() {
+    var container = document.querySelector( 'div#menu-click3' ),
+        triggerBttn = document.getElementById( 'anchor-filter-action-settings-icon' ),
+        overlay = document.querySelector( 'div#action-filter-url-settings' ),
+        closeBttn = overlay.querySelector( 'button.settings-overlay-close' ),
+        closeAnch = overlay.querySelector( 'a.settings-overlay-close' );
+    transEndEventNames = {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+    },
+        transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+        support = { transitions : Modernizr.csstransitions };
+
+    function toggleOverlay() {
+        if( classie.has( overlay, 'open' ) ) {
+            classie.remove( overlay, 'open' );
+            classie.remove( container, 'overlay-open' );
+            classie.add( overlay, 'close' );
+            var onEndTransitionFn = function( ev ) {
+                if( support.transitions ) {
+                    if( ev.propertyName !== 'visibility' ) return;
+                    this.removeEventListener( transEndEventName, onEndTransitionFn );
+                }
+                classie.remove( overlay, 'close' );
+            };
+            if( support.transitions ) {
+                overlay.addEventListener( transEndEventName, onEndTransitionFn );
+            }
+            else {
+                onEndTransitionFn();
+            }
+        }
+        else if( !classie.has( overlay, 'close' ) ) {
+            classie.add( overlay, 'open' );
+            classie.add( container, 'overlay-open' );
+        }
+    }
+
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    closeBttn.addEventListener( 'click', toggleOverlay );
+    closeAnch.addEventListener( 'click', toggleOverlay );
+})();
